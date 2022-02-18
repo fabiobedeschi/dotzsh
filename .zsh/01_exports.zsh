ff5fdb1161171c0b09a7ce6c300a1c85108314e9
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2022-02-18 10:08:25 +0100

    update exports

--- .zsh/01_exports.zsh
+++ .zsh/01_exports.zsh
@@ -8,19 +8,18 @@ fi
 # Paginator
 if type bat &>/dev/null; then
 	export CAT='bat -p'
+	export PAGER=$CAT
 elif type batcat &>/dev/null; then
 	alias bat=batcat
 	export CAT='bat -p'
+	export PAGER=$CAT
 else
 	export CAT='cat'
+	export PAGER='less'
 fi
 
-export PAGER='less'
-export LESS='-FR'
+export LESS='-FR --mouse'
 export AWS_PAGER=$PAGER
-
-# mobymac dockerhost
-# export DOCKER_HOST='tcp://192.168.56.2:2376'
 
 # Color howdoi prompt
 export HOWDOI_COLORIZE=1
