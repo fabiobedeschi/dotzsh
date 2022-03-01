bc227ba220858f9fc956fba58debc6f38f736255
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2022-03-01 09:42:08 +0100

    update brew path

--- .zsh/10_brew.zsh
+++ .zsh/10_brew.zsh
@@ -4,6 +4,8 @@ if type brew &>/dev/null; then
 		$(brew --prefix libpq)/bin
 		$(brew --prefix bzip2)/bin
 		$(brew --prefix coreutils)/libexec/gnubin
+		$(brew --prefix grep)/libexec/gnubin
+		$(brew --prefix)/bin
 		$path
 	)
 fi
--- .zsh/41_ggrep.zsh
+++
@@ -1,4 +0,0 @@
-if type ggrep &> /dev/null; then
-    # Alias for GNU grep
-    alias grep='ggrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
-fi
