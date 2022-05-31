f022a37fe31d3179e6fd992c0dfd264225a2c295
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2022-05-31 12:41:21 +0200

    add 'phpenv install' alias with proper configurations

--- .zsh/64_phpenv.zsh
+++ .zsh/64_phpenv.zsh
@@ -7,4 +7,7 @@ if type phpenv &>/dev/null; then
     if [ ! -d "$HOME/.phpenv" ]; then
         ln -sFi "$PHPENV_ROOT" "$HOME/.phpenv"
     fi
+
+    # phpenv install alias
+    alias 'phpenv install'='CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-bz2=$(brew --prefix bzip2) --with-curl=$(brew --prefix curl) --with-iconv=$(brew --prefix libiconv) --with-libedit=$(brew --prefix libedit) --with-readline=$(brew --prefix readline) --with-tidy=$(brew --prefix tidy-html5)" phpenv install'
 fi
