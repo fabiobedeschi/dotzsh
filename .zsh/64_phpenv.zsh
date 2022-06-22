d7af2de1abc3ce7b11c23dafe7dbf03039a5802e
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2022-06-22 12:48:50 +0200

    temporarely removed "phpenv install" alias

--- .zsh/64_phpenv.zsh
+++ .zsh/64_phpenv.zsh
@@ -9,5 +9,5 @@ if type phpenv &>/dev/null; then
     fi
 
     # phpenv install alias
-    alias 'phpenv install'='CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-bz2=$(brew --prefix bzip2) --with-curl=$(brew --prefix curl) --with-iconv=$(brew --prefix libiconv) --with-libedit=$(brew --prefix libedit) --with-readline=$(brew --prefix readline) --with-tidy=$(brew --prefix tidy-html5)" phpenv install'
+    # alias 'phpenv install'='CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-bz2=$(brew --prefix bzip2) --with-curl=$(brew --prefix curl) --with-iconv=$(brew --prefix libiconv) --with-libedit=$(brew --prefix libedit) --with-readline=$(brew --prefix readline) --with-tidy=$(brew --prefix tidy-html5)" phpenv install'
 fi
