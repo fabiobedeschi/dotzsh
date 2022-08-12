da5485710d6805c8099c3317a12ee413a0cb23ae
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2022-08-12 18:34:39 +0200

    improve broot plugin

--- .zsh/31_broot.zsh
+++ .zsh/31_broot.zsh
@@ -1,1 +1,16 @@
-[[ -r "$HOME/.config/broot/launcher/bash/br" ]] && source $HOME/.config/broot/launcher/bash/br
+if type broot &> /dev/null; then
+	br () {
+		local cmd cmd_file code
+		cmd_file=$(mktemp) 
+		if broot --outcmd "$cmd_file" "$@"
+		then
+			cmd=$(<"$cmd_file") 
+			rm -i -f "$cmd_file"
+			eval "$cmd"
+		else
+			code=$? 
+			rm -i -f "$cmd_file"
+			return "$code"
+		fi
+	}
+fi
