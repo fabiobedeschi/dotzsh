305460ef9e6a72d0fd165cd4b215d075fde2b29a
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2023-03-03 15:25:25 +0100

    update junest path only if installed

--- .zsh/82_junest.zsh
+++ .zsh/82_junest.zsh
@@ -1,2 +1,4 @@
-export PATH="$HOME/.local/share/junest/bin:$PATH"
-export PATH="$PATH:$HOME/.junest/usr/bin_wrappers"
+if type junest &> /dev/null; then
+	export PATH="$HOME/.local/share/junest/bin:$PATH"
+	export PATH="$PATH:$HOME/.junest/usr/bin_wrappers"
+fi
