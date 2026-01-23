d365cbbebee2f300a96be6777da8eedd0a2bb8dd
Fabio Bedeschi <fabiobedeschi5@gmail.com>
2026-01-23 16:44:01 +0100

    update aws plugin

--- .zsh/100_brew.zsh
+++ .zsh/100_brew.zsh
@@ -3,9 +3,9 @@ if type brew &>/dev/null; then
 	path=(
 #		$HOMEBREW_PREFIX/opt/libpq/bin
 #		$HOMEBREW_PREFIX/opt/bzip2/bin
-#		$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
-#		$HOMEBREW_PREFIX/opt/grep/libexec/gnubin
-#		$HOMEBREW_PREFIX/opt/make/libexec/gnubin
+		$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
+		$HOMEBREW_PREFIX/opt/grep/libexec/gnubin
+		$HOMEBREW_PREFIX/opt/make/libexec/gnubin
 		$HOMEBREW_PREFIX/bin
 		$path
 	)
